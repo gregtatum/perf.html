6814025e05693eb81ea581acd754a50e7dd771df
Greg Tatum <tatum.creative@gmail.com>
2017-05-10 10:34:35 -0500

    Make func filtering generic for charging, by implementation, and pruning

--- src/content/reducers/profile-view.js
+++ src/content/reducers/profile-view.js
@@ -457,7 +457,7 @@ export const selectorsForThread = (threadIndex: ThreadIndex): SelectorsForThread
     const _getImplementationFilteredThread = createSelector(
       _getRangeAndCallTreeFilteredThread,
       URLState.getImplementationFilter,
-      ProfileData.filterThreadByImplementation
+      (thread, implementation) => ProfileData.filterThreadByFunc(thread, implementation, [], [])
     );
     const _getImplementationAndSearchFilteredThread = createSelector(
       _getImplementationFilteredThread,
