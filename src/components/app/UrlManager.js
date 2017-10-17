b17d1bed9ca36697dde945a2af174cf3ebf57715
Greg Tatum <tatum.creative@gmail.com>
2017-10-17 17:16:10 -0500

    Add page and event tracking

--- src/components/app/UrlManager.js
+++ src/components/app/UrlManager.js
@@ -7,8 +7,7 @@
 import React, { PureComponent, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { getIsUrlSetupDone } from '../../reducers/app';
-
-import type { Dispatch } from '../../types/store';
+import { updateUrlState, urlSetupDone, show404 } from '../../actions/app';
 
 type Props = {
   stateFromLocation: Location => any,
@@ -39,11 +38,9 @@ class UrlManager extends PureComponent {
     }
   }
   componentDidMount() {
-    const { urlSetupDone } = this.props;
-
     this._updateState();
     window.addEventListener('popstate', () => this._updateState());
-    urlSetupDone();
+    this.props.urlSetupDone();
   }
 
   componentWillReceiveProps(nextProps: Props) {
@@ -82,10 +79,9 @@ export default connect(
     urlState: state.urlState,
     isUrlSetupDone: getIsUrlSetupDone(state),
   }),
-  (dispatch: Dispatch) => ({
-    updateUrlState: urlState =>
-      dispatch({ type: '@@urlenhancer/updateUrlState', urlState }),
-    urlSetupDone: () => dispatch({ type: '@@urlenhancer/urlSetupDone' }),
-    show404: url => dispatch({ type: 'ROUTE_NOT_FOUND', url }),
-  })
+  {
+    updateUrlState,
+    urlSetupDone,
+    show404,
+  }
 )(UrlManager);
