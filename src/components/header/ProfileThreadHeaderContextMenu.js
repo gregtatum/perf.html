a7534a55e411d7c6ba587ba2061cee99accef0cf
Greg Tatum <tatum.creative@gmail.com>
2017-11-02 11:53:31 -0500

    Address review

--- src/components/header/ProfileThreadHeaderContextMenu.js
+++ src/components/header/ProfileThreadHeaderContextMenu.js
@@ -36,18 +36,16 @@ class ProfileThreadHeaderContextMenu extends PureComponent {
   _toggleThreadVisibility(
     _,
     data: {
-      threadOrder: ThreadIndex[],
-      hiddenThreads: ThreadIndex[],
       threadIndex: ThreadIndex,
       isHidden: boolean,
     }
   ): void {
-    const { threadOrder, hiddenThreads, threadIndex, isHidden } = data;
+    const { threadIndex, isHidden } = data;
     const { hideThread, showThread } = this.props;
     if (isHidden) {
       showThread(threadIndex);
     } else {
-      hideThread(threadIndex, threadOrder, hiddenThreads);
+      hideThread(threadIndex);
     }
   }
 
@@ -62,7 +60,7 @@ class ProfileThreadHeaderContextMenu extends PureComponent {
             <MenuItem
               key={threadIndex}
               preventClose={true}
-              data={{ threadOrder, hiddenThreads, threadIndex, isHidden }}
+              data={{ threadIndex, isHidden }}
               onClick={this._toggleThreadVisibility}
               attributes={{
                 className: classNames({ checkable: true, checked: !isHidden }),
