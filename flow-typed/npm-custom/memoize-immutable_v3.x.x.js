858f77d9584ceaf2378d5556b47eee9418cfa351
Greg Tatum <tatum.creative@gmail.com>
2018-01-22 14:49:33 -0600

    Fixup WeakTupleMap for review

--- flow-typed/npm-custom/memoize-immutable_v3.x.x.js
+++ flow-typed/npm-custom/memoize-immutable_v3.x.x.js
@@ -4,5 +4,26 @@
 // @flow
 
 declare module 'memoize-immutable' {
-  declare module.exports: <T: Function>(fn: T, config: Object) => T;
+  declare interface CacheInstance<K, V> {
+    has(K): boolean,
+    get(K): V,
+    set(K, V): void,
+  }
+
+  // TODO also support caches that come from immutable
+  declare type CacheConfig<F> = {|
+    cache: CacheInstance<$Call<ExtractArgType, F>, $Call<ExtractReturnType, F>>,
+  |};
+
+  declare type LimitConfig = {|
+    limit: number,
+  |};
+
+  declare type ExtractArgType = <A>((...rest: A) => any) => A;
+  declare type ExtractReturnType = <V>((...rest: any) => V) => V;
+
+  declare module.exports: <F: Function>(
+    fn: F,
+    config?: CacheConfig<F> | LimitConfig
+  ) => F;
 }
