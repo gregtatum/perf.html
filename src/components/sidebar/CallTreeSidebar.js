d9a189a65f01216f157c8af881518194f35e70ee
Greg Tatum <tatum.creative@gmail.com>
2018-05-21 10:28:53 -0500

    WIP - Breakdown JS by implementation

--- src/components/sidebar/CallTreeSidebar.js
+++ src/components/sidebar/CallTreeSidebar.js
@@ -11,11 +11,19 @@ import { getFunctionName } from '../../profile-logic/function-info';
 
 import type { ExplicitConnectOptions } from '../../utils/connect';
 import type { CallTree } from '../../profile-logic/call-tree';
-import type { IndexIntoCallNodeTable } from '../../types/profile-derived';
+import type { Thread, IndexIntoStackTable } from '../../types/profile';
+import type {
+  IndexIntoCallNodeTable,
+  CallNodeInfo,
+  CallNodePath,
+} from '../../types/profile-derived';
 
 type StateProps = {|
+  +callNodeInfo: CallNodeInfo,
   +tree: CallTree,
+  +thread: Thread,
   +selectedNodeIndex: IndexIntoCallNodeTable | null,
+  +selectedCallNodePath: CallNodePath,
 |};
 
 type SidebarDetailProps = {|
@@ -33,6 +41,106 @@ function SidebarDetail({ label, children }: SidebarDetailProps) {
 }
 
 class CallTreeSidebar extends React.PureComponent<StateProps> {
+  renderJsStackInformation() {
+    const {
+      selectedNodeIndex,
+      thread,
+      callNodeInfo,
+      selectedCallNodePath,
+    } = this.props;
+    if (selectedNodeIndex === null) {
+      return null;
+    }
+
+    const funcIndex = callNodeInfo.callNodeTable.func[selectedNodeIndex];
+    const isJs = thread.funcTable.isJS[funcIndex];
+    if (!isJs) {
+      return null;
+    }
+
+    /*
+    const { funcTable, stackTable, frameTable } = thread;
+    const funcIndex = callNodeInfo.callNodeTable.func[selectedNodeIndex];
+    const isJs = funcTable.isJS[funcIndex];
+    if (!isJs) {
+      return null;
+    }
+
+    // Get all JS frames for a given CallNodePath
+    const stackIndexToCallNodeDepth: Map<
+      IndexIntoStackTable | null,
+      number
+    > = new Map();
+    const stacksMatchingPath: Set<IndexIntoStackTable | null> = new Set();
+    const tipCallNodeDepth = selectedCallNodePath.length - 1;
+    stackIndexToCallNodeDepth.set(null, -1);
+    stacksMatchingPath.add(null);
+    const frameIndexes = [];
+    for (let stackIndex = 0; stackIndex < stackTable.length; stackIndex++) {
+      const prefixIndex = stackTable.prefix[stackIndex];
+      if (!stacksMatchingPath.has(prefixIndex)) {
+        continue;
+      }
+      const depth = stackIndexToCallNodeDepth.get(prefixIndex) + 1;
+      const frameIndex = stackTable.frame[stackIndex];
+      const funcIndex = frameTable.func[frameIndex];
+
+      if (funcIndex === selectedCallNodePath[depth]) {
+        if (depth === tipCallNodeDepth) {
+          // This stack is at the tip of the path, remember the stack frame.
+          frameIndexes.push(frameIndex);
+        } else {
+          // This stack is part of the path, but not at the tip.
+          stacksMatchingPath.add(stackIndex);
+        }
+      }
+    }
+    */
+    const frameType: Map<IndexIntoStackTable, string> = new Map();
+    for (
+      let stackIndex = 0;
+      stackIndex < callNodeInfo.stackIndexToCallNodeIndex.length;
+      stackIndex++
+    ) {
+      if (
+        callNodeInfo.stackIndexToCallNodeIndex[stackIndex] === selectedNodeIndex
+      ) {
+        const frameIndex = thread.stackTable.frame[stackIndex];
+        const stringIndex = thread.frameTable.implementation[frameIndex];
+        const implementation =
+          stringIndex === null
+            ? 'interpreter'
+            : thread.stringTable.getString(stringIndex);
+        frameType.set(stackIndex, implementation);
+      }
+    }
+
+    const sampleCount = {};
+    for (
+      let sampleIndex = 0;
+      sampleIndex < thread.samples.length;
+      sampleIndex++
+    ) {
+      const stackIndex = thread.samples.stack[sampleIndex];
+      if (stackIndex === null) {
+        continue;
+      }
+      const implementation = frameType.get(stackIndex);
+      if (implementation) {
+        sampleCount[implementation] = (sampleCount[implementation] || 0) + 1;
+      }
+    }
+
+    return (
+      <div className="sidebarJsStack">
+        This is JS
+        <div className="sidebarJsStackFrames">
+          Frame indexes: {Object.entries(sampleCount).toString()}
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const { tree, selectedNodeIndex } = this.props;
     if (selectedNodeIndex === null) {
@@ -64,6 +172,7 @@ class CallTreeSidebar extends React.PureComponent<StateProps> {
           <SidebarDetail label="Running Time">{data.totalTime}ms</SidebarDetail>
           <SidebarDetail label="Self Time">{selfTime}</SidebarDetail>
         </div>
+        {this.renderJsStackInformation()}
       </aside>
     );
   }
@@ -71,8 +180,13 @@ class CallTreeSidebar extends React.PureComponent<StateProps> {
 
 const options: ExplicitConnectOptions<{||}, StateProps, {||}> = {
   mapStateToProps: state => ({
+    thread: selectedThreadSelectors.getFilteredThread(state),
+    callNodeInfo: selectedThreadSelectors.getCallNodeInfo(state),
     tree: selectedThreadSelectors.getCallTree(state),
     selectedNodeIndex: selectedThreadSelectors.getSelectedCallNodeIndex(state),
+    selectedCallNodePath: selectedThreadSelectors.getSelectedCallNodePath(
+      state
+    ),
   }),
   component: CallTreeSidebar,
 };
