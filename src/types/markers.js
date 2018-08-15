96973ce4ee2e878c83a7bdda086747c2cb5c95a2
Greg Tatum <tatum.creative@gmail.com>
2018-08-15 14:15:21 -0500

    Extract screenshot tracks from the profile

--- src/types/markers.js
+++ src/types/markers.js
@@ -5,7 +5,7 @@
 
 import type { Milliseconds, Microseconds, Seconds } from './units';
 import type { GeckoMarkerStack } from './gecko-profile';
-import type { IndexIntoStackTable } from './profile';
+import type { IndexIntoStackTable, IndexIntoStringTable } from './profile';
 
 /**
  * Measurement for how long draw calls take for the compositor.
@@ -345,6 +345,18 @@ type VsyncTimestampPayload = {|
   category: 'VsyncTimestamp',
   vsync: 0,
 |};
+
+export type ScreenshotPayload = {
+  // This field represents the data url of the image. It is saved in the string table.
+  url: IndexIntoStringTable,
+  // A memory address that can uniquely identify a window. It has no meaning other than
+  // a way to identify a window.
+  windowID: string,
+  // The original dimensions of the window that was captured. The actual image that is
+  // stored in the string table will be scaled down from the original size.
+  windowWidth: number,
+  windowHeight: number,
+};
 
 /**
  * The payload for Styles.
