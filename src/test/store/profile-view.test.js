72d19d0fbc04c7e9a40945f15e62566e8342fccf
Greg Tatum <tatum.creative@gmail.com>
2018-08-29 14:58:21 -0500

    Update tests for the TracingMarkers and MarkerTable component

--- src/test/store/profile-view.test.js
+++ src/test/store/profile-view.test.js
@@ -919,10 +919,10 @@ describe('snapshots of selectors/profile-view', function() {
       selectedThreadSelectors.getJankInstances(getState())
     ).toMatchSnapshot();
   });
-  it('matches the last stored run of selectedThreadSelector.getProcessedMarkersThread', function() {
+  it('matches the last stored run of selectedThreadSelector.getProcessedMarkersTable', function() {
     const { getState } = setupStore();
     expect(
-      selectedThreadSelectors.getProcessedMarkersThread(getState())
+      selectedThreadSelectors.getProcessedMarkersTable(getState())
     ).toMatchSnapshot();
   });
   it('matches the last stored run of selectedThreadSelector.getTracingMarkers', function() {
@@ -1022,10 +1022,10 @@ describe('snapshots of selectors/profile-view', function() {
       selectedThreadSelectors.getThreadProcessDetails(getState())
     ).toMatchSnapshot();
   });
-  it('matches the last stored run of selectedThreadSelector.getSearchFilteredMarkers', function() {
+  it('matches the last stored run of selectedThreadSelector.getSearchFilteredTracingMarkers', function() {
     const { getState } = setupStore();
     expect(
-      selectedThreadSelectors.getSearchFilteredMarkers(getState())
+      selectedThreadSelectors.getSearchFilteredTracingMarkers(getState())
     ).toMatchSnapshot();
   });
   it('matches the last stored run of selectedThreadSelector.unfilteredSamplesRange', function() {
