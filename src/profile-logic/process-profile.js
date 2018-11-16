be5eacd049b6ecba6ef6a124033c8e0a4a8fdb5b
Greg Tatum <tatum.creative@gmail.com>
2018-11-16 14:00:21 -0600

    Rewrite the JS tracer loading logic to used a keyed component

--- src/profile-logic/process-profile.js
+++ src/profile-logic/process-profile.js
@@ -1002,13 +1002,6 @@ export function serializeProfile(
         }
       }
       newThread.stringArray = stringArray;
-
-      if (thread.jsTracer) {
-        const deserializedJsTracer: Object = { ...thread.jsTracer };
-        deserializedJsTracer.stringArray = deserializedJsTracer.stringTable.serializeToArray();
-        delete deserializedJsTracer.stringTable;
-        newThread.jsTracer = deserializedJsTracer;
-      }
       return newThread;
     }),
   });
