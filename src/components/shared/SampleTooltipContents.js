3b583352c5f284fa9da18af78a6cf68e663f6d6c
Greg Tatum <tatum.creative@gmail.com>
2018-12-12 14:05:28 -0600

    Add a tooltip for samples in the thread activity graph

---
+++ src/components/shared/SampleTooltipContents.js
@@ -0,0 +1,64 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @flow
+
+import * as React from 'react';
+import type {
+  IndexIntoSamplesTable,
+  CategoryList,
+  Thread,
+} from '../../types/profile';
+
+type Props = {|
+  +sampleIndex: IndexIntoSamplesTable,
+  +categories: CategoryList,
+  +fullThread: Thread,
+|};
+
+/**
+ * This class displays the tooltip contents for a given sample. Typically the user
+ * will want to know what the function is, and its category.
+ */
+export default class SampleTooltipContents extends React.PureComponent<Props> {
+  render() {
+    const { sampleIndex, fullThread, categories } = this.props;
+    const {
+      samples,
+      stackTable,
+      frameTable,
+      funcTable,
+      stringTable,
+    } = fullThread;
+    const stackIndex = samples.stack[sampleIndex];
+    if (stackIndex === null) {
+      return 'No stack information';
+    }
+    const categoryIndex = stackTable.category[stackIndex];
+    const category = categories[categoryIndex];
+    const frameIndex = stackTable.frame[stackIndex];
+    const funcIndex = frameTable.func[frameIndex];
+    const nameIndex = funcTable.name[funcIndex];
+    const name = stringTable.getString(nameIndex);
+
+    return (
+      <>
+        <div className="tooltipDetails">
+          <div className="tooltipLabel">Function:</div>
+          <div className="tooltipFunctionName">{name}</div>
+        </div>
+        <div className="tooltipDetails">
+          <div className="tooltipLabel">Category:</div>
+          <div>
+            <span
+              className={`treeViewCategoryKnob category-color-${
+                category.color
+              }`}
+            />
+            {category.name}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
