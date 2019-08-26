b4d3a19f6fc7f1ee287b50dd74359cb7eb34fa54
Greg Tatum <tatum.creative@gmail.com>
2019-08-26 09:30:07 -0500

    Add marker categories from the gecko profiles

--- src/selectors/per-thread/markers.js
+++ src/selectors/per-thread/markers.js
@@ -93,7 +93,9 @@ export function getMarkerSelectorsPerThread(threadSelectors: *) {
    */
   const _getDerivedJankMarkers: Selector<Marker[]> = createSelector(
     threadSelectors.getSamplesTable,
-    samples => MarkerData.deriveJankMarkers(samples, 50)
+    ProfileSelectors.getDefaultCategory,
+    (samples, defaultCategory) =>
+      MarkerData.deriveJankMarkers(samples, 50, defaultCategory)
   );
 
   /**
@@ -332,6 +334,7 @@ export function getMarkerSelectorsPerThread(threadSelectors: *) {
   > = createSelector(
     getMarkerGetter,
     getSearchFilteredMarkerChartMarkerIndexes,
+    ProfileSelectors.getCategories,
     MarkerTimingLogic.getMarkerTimingAndBuckets
   );
 
