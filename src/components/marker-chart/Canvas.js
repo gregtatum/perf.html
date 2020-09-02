39e0c2f801240fe6f2bb96b1c3c7e33ea3b48196
Greg Tatum <tatum.creative@gmail.com>
2020-09-02 12:58:09 -0500

    Add a restricted height and width strategy for tooltips
    This option is set up in the props, as it will be enabled for tooltips,
    and disabled for the sidebar. Restricting the width of the tooltip helps
    it to be more stable in its layout across different markers. This
    creates a nicer experience for the user.
    
    The stacks were using a fading out strategy, but I felt it was better to
    just provide an ellipsis "..." in order to show that the stack had been
    truncated. In addition, I moved around the stack to be more in line with
    the other pieces of the tooltip.

--- src/components/marker-chart/Canvas.js
+++ src/components/marker-chart/Canvas.js
@@ -526,7 +526,11 @@ class MarkerChartCanvas extends React.PureComponent<Props, State> {
 
     const marker = this.props.getMarker(markerIndex);
     return (
-      <TooltipMarker marker={marker} threadIndex={this.props.threadIndex} />
+      <TooltipMarker
+        marker={marker}
+        threadIndex={this.props.threadIndex}
+        restrictHeightWidth={true}
+      />
     );
   };
 
