f24a3c67b5ec6fd8da7021d7a2e6bc3f977cc6dc
Greg Tatum <tatum.creative@gmail.com>
2020-11-13 10:12:55 -0600

    (Do not review, PR #3050) Remove most uses of the Object and Function type.

--- src/test/setup.js
+++ src/test/setup.js
@@ -11,7 +11,7 @@ import * as WorkerFactory from '../utils/worker-factory';
 afterEach(function() {
   // This `__shutdownWorkers` function only exists in the mocked test environment,
   // do not use flow typing on it.
-  const { __shutdownWorkers } = (WorkerFactory: Object);
+  const { __shutdownWorkers } = (WorkerFactory: any);
   __shutdownWorkers();
 });
 
--- src/test/store/receive-profile.test.js
+++ src/test/store/receive-profile.test.js
@@ -1265,8 +1265,8 @@ describe('actions/receive-profile', function() {
       expect(view.phase).toBe('FATAL_ERROR');
 
       expect(
-        // Coerce into the object to access the error property.
-        (view: Object).error
+        // Coerce into an any to access the error property.
+        (view: any).error
       ).toMatchSnapshot();
     });
 
@@ -1371,8 +1371,8 @@ describe('actions/receive-profile', function() {
       });
       expect(view.phase).toBe('FATAL_ERROR');
       expect(
-        // Coerce into the object to access the error property.
-        (view: Object).error
+        // Coerce into an any to access the error property.
+        (view: any).error
       ).toMatchSnapshot();
     });
   });
@@ -1743,7 +1743,10 @@ describe('actions/receive-profile', function() {
       };
     }
 
-    async function setup(location: Object, requiredProfile: number = 1) {
+    async function setup(
+      location: $Shape<Location>,
+      requiredProfile: number = 1
+    ) {
       const profile = _getSimpleProfile();
       const geckoProfile = createGeckoProfile();
 
